1d882e8e3d1534c8f1aec7ed22b8f27054f9b9a3
Mario2701 <77269474@continental.edu.pe>
2022-09-04 15:54:03 -0500

    feat: Update js/modules/slider.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 'use-strict'
 
 import header from "./modules/header.js";
+import sliderJS from "./modules/slider.js";
 
 const readyDocument = () => {
   header();
+  sliderJS();
 };
 
 document.addEventListener('DOMContentLoaded', readyDocument);
