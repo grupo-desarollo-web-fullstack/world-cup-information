9c55dd83ec7d41db97c5d4e50c711f5a6eca5f0c
alejooroncoy <adoa2705@gmail.com>
2022-09-25 10:26:39 -0500

    Update router | Scroll Restoration  ⚛👨‍💻

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,21 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import MoreInfoQatar from "../pages/MoreInfoQatar";
 import WorldCupInformation from "../pages/WorldCupInformation";
 import Simulator from "../pages/Simulator";
 import Layout from "../layout/Layout";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<WorldCupInformation />} />
-          <Route path="more-info-qatar" element={<MoreInfoQatar />} />
-          <Route path="simulator-r" element={<Simulator />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WorldCupInformation />} />
+        <Route path="more-info-qatar" element={<MoreInfoQatar />} />
+        <Route path="simulator-r" element={<Simulator />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
